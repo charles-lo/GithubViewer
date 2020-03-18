caced7ec2e6670806b2f752a43cee48e6a690dc3
Charles Lo <jerries.lo@gmail.com>
2020-03-18 21:20:36 +0800

    rename some inappropriate strings

--- githubViewer/src/androidTest/java/com/charles/githubviewer/ExampleInstrumentedTest.java
+++ githubViewer/src/androidTest/java/com/charles/githubviewer/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.charles.dramalist", appContext.getPackageName());
+        assertEquals("com.charles.githublist", appContext.getPackageName());
     }
 }
